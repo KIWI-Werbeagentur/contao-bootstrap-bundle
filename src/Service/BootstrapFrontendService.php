295a76fe60376cfb4e4a39629e18cbb41ccf4012
Kate <kzimmermann@kiwi.de>
2025-01-06 17:21:14 +0100

    set col classes only when parent rowcols-settings shall be overwritten

--- src/Service/BootstrapFrontendService.php
+++ src/Service/BootstrapFrontendService.php
@@ -6,6 +6,22 @@ use Kiwi\Contao\ResponsiveBaseBundle\Service\ResponsiveFrontendService;
 
 class BootstrapFrontendService extends ResponsiveFrontendService
 {
+    public function getColClasses($strData, $arrData = []): array
+    {
+        if ($arrData['responsiveOverwriteRowCols'] ?? true) {
+            return $this->getResponsiveClasses($strData, 'varColClasses');
+        }
+        return [];
+    }
+
+    public function getOffsetClasses($strData, $arrData = []): array
+    {
+        if ($arrData['responsiveOverwriteRowCols'] ?? true) {
+            return $this->getResponsiveClasses($strData, 'varOffsetClasses');
+        }
+        return [];
+    }
+
     public function getRowColsClasses($strData): array
     {
         return $this->getResponsiveClasses($strData, 'varRowColsClasses');
@@ -20,6 +36,6 @@ class BootstrapFrontendService extends ResponsiveFrontendService
             $this->getRowColsClasses($arrData[$arrFields['rowCols'] ?? 'responsiveRowCols'] ?? "")
         );
 
-        return array_merge($arrBootstrapClasses,parent::getAllInnerContainerClasses($arrData, $arrFields));
+        return array_merge($arrBootstrapClasses, parent::getAllInnerContainerClasses($arrData, $arrFields));
     }
 }
