a742d72663f91a0e9a384330ffd8e59887e78fa1
Patrick Josupeit <pjosupeit@kiwi.de>
2026-05-08 15:32:01 +0200

    add missing / update english translations

--- contao/languages/en/responsive.php
+++ contao/languages/en/responsive.php
@@ -1,5 +1,19 @@
 <?php
 
+$GLOBALS['TL_LANG']['responsive']['flexContent']['between'] = "Distributed [space-between]";
+$GLOBALS['TL_LANG']['responsive']['flexContent']['around'] = "Distributed with half space outside [space-around]";
+$GLOBALS['TL_LANG']['responsive']['flexContent']['evenly'] = "Distributed with space outside [space-evenly]";
+
+$GLOBALS['TL_LANG']['responsive']['spacings']['default'][0] = "Default [default]";
+$GLOBALS['TL_LANG']['responsive']['spacings']['none'][0] = "No spacing [none]";
+$GLOBALS['TL_LANG']['responsive']['spacings']['gap'][0] = "Half grid gap [gap]";
+$GLOBALS['TL_LANG']['responsive']['spacings']['gap-full'][0] = "Full grid gap [gap-full]";
+$GLOBALS['TL_LANG']['responsive']['spacings']['xs'][0] = "Extra small [xs]";
+$GLOBALS['TL_LANG']['responsive']['spacings']['sm'][0] = "Small [sm]";
+$GLOBALS['TL_LANG']['responsive']['spacings']['md'][0] = "Medium [md]";
+$GLOBALS['TL_LANG']['responsive']['spacings']['lg'][0] = "Large [lg]";
+$GLOBALS['TL_LANG']['responsive']['spacings']['xl'][0] = "Extra large [xl]";
+
 $GLOBALS['TL_LANG']['responsive']['breakpoint']['xs'][0] = "Default (Smartphone)";
 $GLOBALS['TL_LANG']['responsive']['breakpoint']['sm'][0] = "Smartphone Landscape";
 $GLOBALS['TL_LANG']['responsive']['breakpoint']['md'][0] = "Tablet";
@@ -7,17 +21,26 @@ $GLOBALS['TL_LANG']['responsive']['breakpoint']['lg'][0] = "Laptop";
 $GLOBALS['TL_LANG']['responsive']['breakpoint']['xl'][0] = "PC";
 $GLOBALS['TL_LANG']['responsive']['breakpoint']['xxl'][0] = "TV";
 
-$GLOBALS['TL_LANG']['responsive']['responsiveRowCols']=[
-    'auto' => "Automatic"
+$GLOBALS['TL_LANG']['responsive']['responsiveRowCols'] = [
+    0 => "Items per row",
+    'auto' => "Automatic [auto]",
+    'options' => [
+        'auto' => "Automatic [auto]",
+    ]
 ];
 
-$GLOBALS['TL_LANG']['responsive']['flexContainer']=[
+$GLOBALS['TL_LANG']['responsive']['flexContainer'] = [
     'default' => "Column-Element",
-    'container-fluid' => "Full Width",
-    'container' => "Limited Width",
-    'container-sm' => "Limited Width at {$GLOBALS['TL_LANG']['responsive']['breakpoint']['sm'][0]}",
-    'container-md' => "Limited Width at {$GLOBALS['TL_LANG']['responsive']['breakpoint']['md'][0]}",
-    'container-lg' => "Limited Width at {$GLOBALS['TL_LANG']['responsive']['breakpoint']['lg'][0]}",
-    'container-xl' => "Limited Width at {$GLOBALS['TL_LANG']['responsive']['breakpoint']['xl'][0]}",
-    'container-xxl' => "Limited Width at {$GLOBALS['TL_LANG']['responsive']['breakpoint']['xxl'][0]}",
+    'container-fluid' => "Full Width <span class='label-info'>[container-fluid]</span>",
+    'container' => "Limited Width <span class='label-info'>[container]</span>",
+    'container-sm' => "Limited Width at {$GLOBALS['TL_LANG']['responsive']['breakpoint']['sm'][0]} <span class='label-info'>[container-sm]</span>",
+    'container-md' => "Limited Width at {$GLOBALS['TL_LANG']['responsive']['breakpoint']['md'][0]} <span class='label-info'>[container-md]</span>",
+    'container-lg' => "Limited Width at {$GLOBALS['TL_LANG']['responsive']['breakpoint']['lg'][0]} <span class='label-info'>[container-lg]</span>",
+    'container-xl' => "Limited Width at {$GLOBALS['TL_LANG']['responsive']['breakpoint']['xl'][0]} <span class='label-info'>[container-xl]</span>",
+    'container-xxl' => "Limited Width at {$GLOBALS['TL_LANG']['responsive']['breakpoint']['xxl'][0]} <span class='label-info'>[container-xxl]</span>",
+];
+
+$GLOBALS['TL_LANG']['responsive']['responsiveOrder']['options'] = [
+    'first' => "First",
+    'last' => "Last"
 ];
