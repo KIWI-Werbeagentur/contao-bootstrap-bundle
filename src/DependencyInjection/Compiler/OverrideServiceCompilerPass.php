fa28bc475a374369183edadebda94c44dc4b443e
Patrick Josupeit <pjosupeit@kiwi.de>
2026-09-22 15:49:59 +0200

    use class constant over hardcoded string

--- src/DependencyInjection/Compiler/OverrideServiceCompilerPass.php
+++ src/DependencyInjection/Compiler/OverrideServiceCompilerPass.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Kiwi\Contao\BootstrapBundle\DependencyInjection\Compiler;
+
+use Kiwi\Contao\BootstrapBundle\Service\BootstrapFrontendService;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
@@ -9,6 +11,6 @@ class OverrideServiceCompilerPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         $defNewService = $container->getDefinition('kiwi.contao.responsive.frontend');
-        $defNewService->setClass('\Kiwi\Contao\BootstrapBundle\Service\BootstrapFrontendService');
+        $defNewService->setClass(BootstrapFrontendService::class);
     }
 }
